b20373a5fbc307cb41e5dd3bc22ef63f56bd09a6
Leticia <lettycuevas91@gmail.com>
2023-01-14 13:47:05 -0300

    test

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.counter= this.counter.bind(this);
+    this.state ={ value :0 }
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+          <textarea rows="3" onKeyUp={this.counter}></textarea>
+        <div className="counter" >{this.state.value}</div>
       </div>
     );
+  }
+  counter(){
+    this.setState({value: this.state.value + 1});
   }
 }
 
